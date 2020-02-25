1320244228a4d37be62942aae62d1889aa000b25
samuel Temitope <stemitope370@gmail.com>
2020-02-25 22:04:10 +0100

    css and displaying questions

--- app.js
+++ app.js
@@ -1,55 +1,10 @@
 
 
 const quizContainer = document.querySelector('#quiz');
-const resultContainer = document.querySelector('#result');
+const resultsContainer = document.querySelector('#result');
 const submitBtn = document.querySelector('#submit');
 
-function buildQuiz(){
-    //storing the html output
 
-    const output = [];
-
-    //loop through the question
-    myQuestions.forEach(
-        (currentQuestion, questionNumber) =>{
-            //store list of answers
-        const answers = [];
-        
-        // available answers
-        for (letter in currentQuestion.answers){
-            ///add the radio button
-            answers.push(
-                `<label>
-                    <input type="radio" name="question${questionNumber}" value="${letter}">
-                        ${letter} : ${currentQuestion.answers[letter]}
-                </label>`
-            );
-        }
-        // add the question and its answer to output
-        output.push(
-            `<div class="question> ${currentQuestion.question}</div>
-            <div class='answers'> ${answers.join('')}</div>`
-        )
-
-        }
-    );
-
-    //combine the output into one string
-
-    quizContainer.innerHTML = output.join('');
-
-}
-
-function showResult(){
-
-}
-
-//display quiz
-buildQuiz();
-
-//show result on submit
-
-submitBtn.addEventListener('click', showResult);
 
 
 //question bank
@@ -82,6 +37,88 @@ const myQuestions = [
             c:'RequireJS',
             d:'ESLint'
         }, 
-        correctAnswer:'D'
+        correctAnswer:'d'
     }
 ]
+
+function buildQuiz(){
+    //storing the html output
+
+    const output = [];
+
+    //loop through the question
+    myQuestions.forEach(
+        (currentQuestion, questionNumber) =>{
+            //store list of answers by candidate
+        const answers = [];
+        
+        // available answers
+        for (letter in currentQuestion.answers){
+            ///add the radio 
+            answers.push(
+                `<label>
+                    <input type="radio" name="question${questionNumber}" value="${letter}">
+                        ${letter} : ${currentQuestion.answers[letter]}
+                </label>`
+            );
+        }
+        // add the question and its answer to output
+        output.push(
+            `<div class="question"> ${currentQuestion.question}</div>
+            <div class='answers'> ${answers.join(' ')}</div>`
+        )
+
+        }
+    );
+
+    //combine the output into one string
+
+    quizContainer.innerHTML = output.join(' ');
+
+}
+
+function showResults(){
+    const answerContainers = quizContainer.querySelectorAll('.answers');
+
+    //monitor users answers
+    let numCorrect = 0;
+
+    //for each question
+    myQuestions.forEach((currentQuestion, questionNumber) => {
+
+        //find selected answers
+
+    const answerContainer = answerContainers[questionNumber];
+    const selector = `input[name=question${questionNumber}]:checked`;
+    const userAnswer = ( answerContainer.querySelector(selector) || {}).value;
+
+
+    ///conditional statements to check the validity of the questions
+    if (userAnswer === currentQuestion.correctAnswer){
+        /// add +1 to  the scores
+        numCorrect++;
+
+
+         //add styles to correct answers
+         answerContainers[questionNumber].style.color="lightgreen";
+    }
+    else{
+       
+        //add styles to the wrong answers
+        answerContainers[questionNumber].style.color="red";
+    }
+
+    });
+
+    //show number of correct answers out of total
+    resultsContainer.innerHTML = `You  got ${numCorrect} out of ${myQuestions.length} Questions`;
+}
+
+//display quiz
+buildQuiz();
+
+//show result on submit
+
+submitBtn.addEventListener('click', showResults);
+
+
